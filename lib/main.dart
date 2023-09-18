86cc5287c4f8c690d29e0614a6383577364cce18
TasnimSariaa <141612798+TasnimSariaa@users.noreply.github.com>
2023-09-19 00:04:37 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,58 +9,281 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true, //also its by default true
       home: HomeScreen(),
     );
   }
 }
 
-
 class HomeScreen extends StatelessWidget {
-
-
-  List<String> items=[
-    'Bananas',
-    'Milk',
-    'Bread',
-    'bag',
-    'shoes',
-
-  ];
+  MySnackBar(massage, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(massage)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("My Shopping List",style: TextStyle(
-            fontWeight: FontWeight.w300,
-            fontSize: 35,
-          ),),
+        appBar: AppBar(
+          title: Text(
+            "Photo Gallery",
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 35,
+            ),
+          ),
           //titleSpacing: 90,
           centerTitle: true,
           toolbarHeight: 70,
           elevation: 70,
           toolbarOpacity: 1,
           backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: 750,
+                  height: 60,
+                  child: Text(
+                    'Welcome to My Photo Gallery!',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  width: 750,
+                  child: TextField(
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: 'Search for photos...',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Scrollbar(
+                    child: SizedBox(
+                  height: 200,
+                  width: 400,
+                  child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3, childAspectRatio: 1.2),
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 0 "),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 100,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 0",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 1"),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 300,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 1",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 2"),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 300,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 2",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 3"),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 300,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 3",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 4"),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 300,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 4",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text("pic 5"),
+                                  ));
+                                },
+                                child: Image.network(
+                                  'https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                                  width: 300,
+                                  height: 60,
+                                )),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Photo 5",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
 
-          actions: [
-            IconButton( icon:Icon(Icons.shopping_cart_rounded),iconSize: 35, onPressed: () { print("Shopping"); },),
-          ]
-      ),
-      body:Scrollbar(
-    child: ListView.builder(
-    itemCount: items.length,
-        itemBuilder: (context,index){
-          return ListTile(
-            title: Text(items[index]),
-            leading: IconButton(onPressed: (){}, icon:Icon(Icons.shopping_basket)),
-          );
-        }
-
-    ),
-
-      )
-
-    );
+                Column(
+                  children: [
+                    ListTile(
+                      title: Text('Photo 1'),
+                      subtitle: Text('Discription for Photo 1'),
+                      leading: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                          ("https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text('Photo 2'),
+                      subtitle: Text('Discription for Photo 2'),
+                      leading: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                          ("https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text('Photo 3'),
+                      subtitle: Text('Discription for Photo 3'),
+                      leading: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                          ("https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Photo Uploaded Successfully!"),
+                          ));
+                        },
+                        child: Icon(Icons.upload, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(15),
+                          backgroundColor: Colors.blue,
+                        ),
+                      ),
+                    )
+                  ],
+                ), //leading: ImageIcon
+              ],
+            ),
+          ),
+        ));
   }
 }
