7c98c80aaf5a9bda3e9d1f7123901e1d5206f119
TasnimSariaa <141612798+TasnimSariaa@users.noreply.github.com>
2023-09-25 00:13:56 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,102 @@
-//
-//sdfghjkasdfghjdfghjk cvbnm//dfghj
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: CounterScreen(),
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        //elevatedButtonTheme: ElevatedButtonThemeData(
 
-//sdfghjk
-//dfgh
+        //)
+      ),
+    );
+  }
+}
+
+//route
+
+class CounterScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CounterState();
+  }
+}
+
+class CounterState extends State<CounterScreen> {
+  int counter = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Counter App",
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 35,
+            ),
+          ),
+          //titleSpacing: 90,
+          centerTitle: true,
+          toolbarHeight: 70,
+          elevation: 70,
+          toolbarOpacity: 1,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(counter.toString()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      counter = counter + 1;
+                      if (counter >= 5) {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                content: Text("Button pressed 5 times"),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text('Cencel')),
+                                ],
+                              );
+                            });
+                      }
+                      print(counter);
+                      setState(() {});
+                    },
+                    child: Icon(Icons.add),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (counter > 0) {
+                        counter = counter - 1;
+                        print(counter);
+                        setState(() {
+                          if (counter == 5) {}
+                        });
+                      }
+                    },
+                    child: Icon(Icons.exposure_minus_1),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
